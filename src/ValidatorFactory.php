81adc641af28e3f6e2750019554eb6a7fc1305c7
Albert Moreno <a.moreno@proengsoft.com>
2015-08-14 03:00:03 +0200

    Validator Factory

--- src/ValidatorFactory.php
+++ src/ValidatorFactory.php
@@ -18,29 +18,9 @@ class ValidatorFactory extends Factory
      */
     public function make(array $data, array $rules, array $messages = array(), array $customAttributes = array())
     {
-        // The presence verifier is responsible for checking the unique and exists data
-        // for the validator. It is behind an interface so that multiple versions of
-        // it may be written besides database. We'll inject it into the validator.
-        $validator = $this->resolve($data, $rules, $messages, $customAttributes);
 
-        if ( ! is_null($this->verifier))
-        {
-            $validator->setPresenceVerifier($this->verifier);
-        }
-
-        // Next we'll set the IoC container instance of the validator, which is used to
-        // resolve out class based validator extensions. If it is not set then these
-        // types of extensions will not be possible on these validation instances.
-        if ( ! is_null($this->container))
-        {
-            $validator->setContainer($this->container);
-        }
-
-        $this->addExtensions($validator);
-
-        $proxyValidator=new ValidatorProxy($validator);
-
-        return $proxyValidator;
+        $validator = parent::make($data, $rules, $messages, $customAttributes);
+        return new ValidatorProxy($validator);
     }
 
 }
